0708f72ab20e7a7131f5a685951ece4b75a93cf5
Janusz Grabis <janusz@frameworks.pl>
2020-09-06 16:42:12 +0200

    Initial commit (one empty working test)

---
+++ findpairs/findpairs.cpp
@@ -0,0 +1,34 @@
+#include "../include/gtest/gtest.h"
+#include <iostream>
+
+// using namespace std;
+// int main()
+// {
+//     const int ARR_SIZE = 8;
+//     int arr[ARR_SIZE] = {1, 1, 3, 4, 5, 6, 7, 7};
+//     
+//     for (int i = 0; i < ARR_SIZE; i++) {
+//         
+//     }
+//}
+
+// class FindPairsTest : public ::testing::Test
+// {
+// 
+// };
+
+TEST(FindPairsTest, calculateSpanBetweenDates)
+{
+// 	CString sStart = _T("2017-11-26 12:00:00");
+// 	CString sEnd = _T("2017-11-26 12:00:30");
+// 
+// 	SYSTEMTIME startST;
+// 	SYSTEMTIME endST;
+// 	fw::core::DateTime::stringToTime(sStart, _T("Y-M-D H:i:s"), &startST);
+// 	fw::core::DateTime::stringToTime(sEnd, _T("Y-M-D H:i:s"), &endST);
+// 
+// 	int iSeconds = fw::core::DateTime::getTimeSpanAsSeconds(startST, endST);
+              
+	ASSERT_EQ(30, 30);
+
+}
