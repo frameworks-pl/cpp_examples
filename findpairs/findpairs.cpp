15bda67f3262a96e04765e2c4324e05cb76c5957
Janusz Grabis <janusz@frameworks.pl>
2020-09-06 18:03:26 +0200

    Testing if values are correct

--- findpairs/findpairs.cpp
+++ findpairs/findpairs.cpp
@@ -1,34 +1,70 @@
 #include "../include/gtest/gtest.h"
 #include <iostream>
+#include <vector>
+#include <utility>
 
-// using namespace std;
-// int main()
-// {
-//     const int ARR_SIZE = 8;
-//     int arr[ARR_SIZE] = {1, 1, 3, 4, 5, 6, 7, 7};
-//     
-//     for (int i = 0; i < ARR_SIZE; i++) {
-//         
-//     }
-//}
 
-// class FindPairsTest : public ::testing::Test
-// {
-// 
-// };
+std::vector<std::pair<int, int>> findPairs(const std::vector<int>& sourceNumbers, const int expectedSum) {
 
-TEST(FindPairsTest, calculateSpanBetweenDates)
-{
-// 	CString sStart = _T("2017-11-26 12:00:00");
-// 	CString sEnd = _T("2017-11-26 12:00:30");
-// 
-// 	SYSTEMTIME startST;
-// 	SYSTEMTIME endST;
-// 	fw::core::DateTime::stringToTime(sStart, _T("Y-M-D H:i:s"), &startST);
-// 	fw::core::DateTime::stringToTime(sEnd, _T("Y-M-D H:i:s"), &endST);
-// 
-// 	int iSeconds = fw::core::DateTime::getTimeSpanAsSeconds(startST, endST);
-              
-	ASSERT_EQ(30, 30);
+	std::vector<std::pair<int, int>> result;
 
+	int forwardIdx = 0;
+	int backwardIdx = (int)sourceNumbers.size()-1;
+
+	while (forwardIdx < sourceNumbers.size() && backwardIdx >= 0 && forwardIdx != backwardIdx) {
+
+		int currentSum = sourceNumbers[forwardIdx] + sourceNumbers[backwardIdx];
+		if (currentSum == expectedSum) {
+			result.push_back(std::pair<int, int>(sourceNumbers[forwardIdx], sourceNumbers[backwardIdx]));
+			forwardIdx++;
+			backwardIdx--;
+		} else {
+			if (currentSum < expectedSum) { forwardIdx++; } else { backwardIdx--;}
+		}
+	
+	}
+
+	return result;
+
+}
+
+TEST(FindPairs, simpleCase) {
+
+	const int EXPECTED_SUM = 8;
+	std::vector<int> sourceNumbers{ 1, 1, 3, 4, 5, 6, 7 };
+	std::vector<std::pair<int, int>> resultPairs = findPairs(sourceNumbers, EXPECTED_SUM);
+
+	ASSERT_EQ(2, resultPairs.size());
+
+	ASSERT_EQ(1, resultPairs[0].first);
+	ASSERT_EQ(7, resultPairs[0].second);
+
+	ASSERT_EQ(3, resultPairs[1].first);
+	ASSERT_EQ(5, resultPairs[1].second);	
+}
+
+TEST(FindPairs, foursInTheMiddle) {
+	const int EXPECTED_SUM = 8;
+	std::vector<int> sourceNumbers{ 1, 1, 3, 4, 4, 4, 5, 6, 7 };
+	std::vector<std::pair<int, int>> resultPairs = findPairs(sourceNumbers, EXPECTED_SUM);
+
+	ASSERT_EQ(3, resultPairs.size());
+
+	ASSERT_EQ(1, resultPairs[0].first);
+	ASSERT_EQ(7, resultPairs[0].second);
+
+	ASSERT_EQ(3, resultPairs[1].first);
+	ASSERT_EQ(5, resultPairs[1].second);		
+}
+
+TEST(FindPairs, foursAtTheEnd) {
+
+	const int EXPECTED_SUM = 8;
+	std::vector<int> sourceNumbers{ 1, 1, 3, 4, 4, 4 };
+	std::vector<std::pair<int, int>> resultPairs = findPairs(sourceNumbers, EXPECTED_SUM);
+
+	ASSERT_EQ(1, resultPairs.size());
+	
+	ASSERT_EQ(4, resultPairs[0].first);
+	ASSERT_EQ(4, resultPairs[0].second);			
 }
