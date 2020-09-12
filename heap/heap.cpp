99ec5d08b1bcaa1bb3640fb52cbca23216aa847b
Janusz Grabis <janusz@frameworks.pl>
2020-09-12 10:15:03 +0200

    Building heap, finding max, sorting

---
+++ heap/heap.cpp
@@ -0,0 +1,35 @@
+#include <../include/gtest/gtest.h>
+#include <vector>
+#include <algorithm>
+
+
+int findMax(std::vector<int>& unsortedNumbers) {
+  
+  std::make_heap(unsortedNumbers.begin(), unsortedNumbers.end());
+  return unsortedNumbers[0];  
+    
+};
+
+int lessThan(int a, int b) {
+    return a < b;
+}
+
+int greaterThan(int a, int b) {
+    return a >= b;
+}
+
+TEST(GenericSuite, findMaxTest) {
+
+    std::vector<int> unsortedNumbers = { 20, 10, 15, 23 };
+    int result = findMax(unsortedNumbers);
+    
+    ASSERT_EQ(23, result);
+    
+    std::sort_heap(unsortedNumbers.begin(), unsortedNumbers.end(), lessThan);
+    ASSERT_EQ(10, unsortedNumbers[0]);
+    
+    std::sort_heap(unsortedNumbers.begin(), unsortedNumbers.end(), greaterThan);
+    ASSERT_EQ(23, unsortedNumbers[0]); 
+ 
+
+}
---
+++ test.bat
@@ -0,0 +1,2 @@
+cl /EHsc /I../src /I.. /I../include /I../include/gtest gtest.cc gtest_main.cc gtest-death-test.cc gtest-filepath.cc gtest-matchers.cc gtest-port.cc gtest-printers.cc gtest-test-part.cc gtest-typed-test.cc ..\%1\%1.cpp
+gtest.exe
