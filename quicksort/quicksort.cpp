497842f9fa8b25881d1614ec9849fe85a43f8f2e
Janusz Grabis <janusz@frameworks.pl>
2020-09-13 07:20:38 +0200

    quick sort implementation

---
+++ quicksort/quicksort.cpp
@@ -0,0 +1,58 @@
+#include "../include/gtest/gtest.h" 
+#include <vector>
+#include <iostream>
+
+int findPivot(const std::vector<int>& unsortedArray, int startIdx, int endIdx) {
+
+    unsigned int firstLastBiggerIdx = unsortedArray[startIdx] > unsortedArray[endIdx] ? startIdx : endIdx;
+    return unsortedArray[firstLastBiggerIdx] > unsortedArray[(endIdx-startIdx)/2] ? firstLastBiggerIdx : (endIdx-startIdx)/2;    
+}
+
+int partition(std::vector<int>& unsortedArray, int startIdx, int endIdx) {
+
+    int pivot = endIdx;
+    int firstHigh = startIdx;
+    for (int i = startIdx; i < endIdx; i++) {
+        if (unsortedArray[i] < unsortedArray[pivot]) {
+            int firstHighVal = unsortedArray[firstHigh];
+            unsortedArray[firstHigh] = unsortedArray[i];
+            unsortedArray[i] = firstHighVal;
+            firstHigh++;    
+        }        
+    }
+    
+    int pivotVal = unsortedArray[pivot];
+    unsortedArray[pivot] = unsortedArray[firstHigh];
+    unsortedArray[firstHigh] = pivotVal;
+    
+    return firstHigh;     
+}
+
+
+void quickSort(std::vector<int>& unsortedArray, int startIdx = -1, int endIdx = -1) {
+
+    if (startIdx == -1 && endIdx == -1) {
+        startIdx = 0;
+        endIdx = (int)unsortedArray.size()-1;
+    }
+
+    if ((endIdx - startIdx) > 0) {
+        int pivot = partition(unsortedArray, startIdx, endIdx);
+        quickSort(unsortedArray, startIdx, pivot-1);
+        quickSort(unsortedArray, pivot+1, endIdx);  
+    } 
+    
+}
+
+
+TEST(QuickSort, basicTest) {
+
+    std::vector<int> unsortedArray = { 3, 5, 2 , 4, 1, 8 };
+    quickSort(unsortedArray);
+    ASSERT_EQ(1, unsortedArray[0]);
+    
+    std::vector<int> unsortedArray2 = { 7, 35, 59, 2, 19, 98, 1005, 587, 74 };
+    quickSort(unsortedArray2);
+    ASSERT_EQ(2, unsortedArray2[0]);
+
+}
--- test.bat
+++ test.bat
@@ -1,2 +1,8 @@
+IF "" == "%DEVENVDIR%" CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+IF "" == "%1" GOTO no_param   
 cl /EHsc /I../src /I.. /I../include /I../include/gtest gtest.cc gtest_main.cc gtest-death-test.cc gtest-filepath.cc gtest-matchers.cc gtest-port.cc gtest-printers.cc gtest-test-part.cc gtest-typed-test.cc ..\%1\%1.cpp
 gtest.exe
+GOTO end
+:no_param
+@ECHO Please provide name of the file to run
+:end
