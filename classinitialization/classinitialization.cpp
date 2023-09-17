dd84022d251d7d03bb2390ca438800d7b547522b
Janusz Grabis <janusz@frameworks.pl>
2023-09-17 09:09:41 +0200

    Class members initialization

---
+++ classinitialization/classinitialization.cpp
@@ -0,0 +1,28 @@
+#include <../include/gtest/gtest.h>
+#include <string>
+#include <vector>
+#include <queue>
+
+class SimpleClass {
+
+    int value2;
+    int value1;
+
+    public:
+        SimpleClass(int _value1) : value1(_value1), value2(_value1+1) {
+
+        }
+
+        std::string getString() {
+            return std::to_string(value1) + " " + std::to_string(value2);
+        }
+
+};
+
+
+TEST(ClassInitialization, membersInitialization) {
+
+    SimpleClass sc(5);
+    ASSERT_EQ("5 6", sc.getString());
+
+}
